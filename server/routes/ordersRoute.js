779646d981d7a1c42efe8f42b866cff61bb49254
Liziane Garcia <lizianegarciaa@gmail.com>
2021-10-26 14:37:36 -0300

    jwt to heroku

--- server/routes/ordersRoute.js
+++ server/routes/ordersRoute.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const ordersController = require('../controller/ordersController');
-const { auth } = require('../controller/authController');
+// const { auth } = require('../controller/authController');
 
 
-router.get('/', auth,  ordersController.getAllOrders);
-router.get('/:orderId', auth, ordersController.getOrderById);
-router.post('/', auth, ordersController.postOrders);
-router.put('/:orderId', auth, ordersController.putOrder);
-router.delete('/:orderId', auth, ordersController.deleteOrder);
+router.get('/', ordersController.getAllOrders);
+router.get('/:orderId', ordersController.getOrderById);
+router.post('/', ordersController.postOrders);
+router.put('/:orderId', ordersController.putOrder);
+router.delete('/:orderId', ordersController.deleteOrder);
 
 module.exports = router;
--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controller/usersController');
-const { auth } = require('../controller/authController');
+// const { auth } = require('../controller/authController');
 
-router.get("/", auth, usersController.getAllUsers);
-router.get("/:uid", auth, usersController.getUserById);
-router.post("/", auth, usersController.postUser);
-router.put("/:uid", auth, usersController.putUser);
-router.delete("/:uid", auth, usersController.deleteUser);
+router.get("/", usersController.getAllUsers);
+router.get("/:uid", usersController.getUserById);
+router.post("/", usersController.postUser);
+router.put("/:uid", usersController.putUser);
+router.delete("/:uid", usersController.deleteUser);
 
 module.exports = router;
