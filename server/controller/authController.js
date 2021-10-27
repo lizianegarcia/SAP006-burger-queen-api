3a352b49c99b66f1e203f0d59edf870e20af91ec
Liziane Garcia <lizianegarciaa@gmail.com>
2021-10-27 19:11:16 -0300

    alterando versão swagger

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -11,9 +11,7 @@ module.exports = {
 
     Users.findOne({
       where: {
-        email: email,
-        password: password,
-        
+        email: email
       },
     })
     .then((user) => {
